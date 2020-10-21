1ea0ab859968338d25e6bb370634b3a401790ca7
Bruno Andreo <brunolino2026@gmail.com>
2020-10-20 21:54:07 -0300

    Adicionando restante das rotas

--- src/controllers/Secrets.ts
+++ src/controllers/Secrets.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { Controller, Get, Post } from '@overnightjs/core';
+import { Controller, Delete, Get, Post, Put } from '@overnightjs/core';
 
 @Controller('secrets')
 export default class Secrets {
@@ -14,4 +14,18 @@ export default class Secrets {
     return res.json({ message: 'Hello Secrets GET' });
   }
 
+  @Get(':id')
+  public getSecretById(req: Request, res: Response): Response {
+    return res.json({ message: `Hello ID ${req.params.id}` });
+  }
+
+  @Put(':id')
+  public updateSecret(req: Request, res: Response): Response {
+    return res.json({ message: `Hello ID ${req.params.id}` });
+  }
+
+  @Delete(':id')
+  public deleteSecret(req: Request, res: Response): Response {
+    return res.json({ message: `Hello ID ${req.params.id}` });
+  }
 }
