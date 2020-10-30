c2a95e9b9963c9609d7103f2e7952c38be6ad21b
Bruno Andreo <brunolino2026@gmail.com>
2020-10-30 09:00:31 -0300

    Refactor em classe de controller

--- src/controllers/Secrets.ts
+++ src/controllers/Secrets.ts
@@ -5,13 +5,22 @@ import SecretsDomain from '../domain/SecretsDomain';
 @Controller('secrets')
 export default class Secrets {
 
+  private secretsDomain: SecretsDomain;
+
+  constructor() {
+    this.secretsDomain = new SecretsDomain();
+  }
+
+  private getIdParam(req: Request): Number {
+    return Number.parseInt(req.params.id);
+  }
+
   @Post()
   public async newSecret(req: Request, res: Response): Promise<Response> {
     try {
       const { name, username, password } = req.body;
 
-      const secretsDomain = new SecretsDomain();
-      const id = await secretsDomain.newSecret(name, username, password);
+      const id = await this.secretsDomain.newSecret(name, username, password);
 
       return res.status(201).json({ idSecret: id });
     } catch (e) {
@@ -22,8 +31,7 @@ export default class Secrets {
   @Get()
   public async getSecrets(req: Request, res: Response): Promise<Response> {
     try {
-      const secretsDomain = new SecretsDomain();
-      const secrets = await secretsDomain.getAllSecrets();
+      const secrets = await this.secretsDomain.getAllSecrets();
 
       return res.json({ secrets });
     } catch (e) {
@@ -34,10 +42,8 @@ export default class Secrets {
   @Get(':id')
   public async getSecretById(req: Request, res: Response): Promise<Response> {
     try {
-      const id = Number.parseInt(req.params.id);
-      const secretsDomain = new SecretsDomain();
-
-      const secret = await secretsDomain.getSecretById(id);
+      const id = this.getIdParam(req);
+      const secret = await this.secretsDomain.getSecretById(id);
 
       return res.json({ secret });
 
@@ -49,11 +55,10 @@ export default class Secrets {
   @Put(':id')
   public async updateSecret(req: Request, res: Response): Promise<Response> {
     try {
-      const id = Number.parseInt(req.params.id);
+      const id = this.getIdParam(req);
       const { name, username, password, active } = req.body;
 
-      const secretsDomain = new SecretsDomain();
-      const secret = await secretsDomain.updateSecret(id, name, username, password, active);
+      const secret = await this.secretsDomain.updateSecret(id, name, username, password, active);
 
       return res.status(200).json({ secret });
     } catch (e) {
@@ -64,10 +69,9 @@ export default class Secrets {
   @Delete(':id')
   public async deleteSecret(req: Request, res: Response): Promise<Response> {
     try {
-      const id = Number.parseInt(req.params.id);
+      const id = this.getIdParam(req);
 
-      const secretsDomain = new SecretsDomain();
-      const success = await secretsDomain.inactiveSecrets(id);
+      const success = await this.secretsDomain.inactiveSecrets(id);
 
       return res.status(200).json({ success });
     } catch (e) {
